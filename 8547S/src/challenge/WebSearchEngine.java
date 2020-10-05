2d97a9c594a5eed13278816840d3ad99a7eb793a
Wen Dong <wen.dong@microfocus.com>
2020-10-05 09:05:37 +0300

    web search engine ready

--- 8547S/src/challenge/WebSearchEngine.java
+++ 8547S/src/challenge/WebSearchEngine.java
@@ -3,9 +3,14 @@
  */
 package challenge;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
+
 import heaps.BinaryHeap;
 import java.io.File;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 /**
  * Given a set of keywords as input, it ranks the web pages based on their score (score = sum of matches per keyword, or keyword * frequency). 
  * The program should list the top 10 pages with the best matches
@@ -29,12 +34,28 @@ public class WebSearchEngine {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		for (String arg:args) {
-			System.out.println(arg);
-		}
+		
+        System.out.println("Enter the keywords to search delimited by space:");
+
+        Scanner sc = new Scanner(System.in);
+        List<String> keywordsList = new ArrayList<String>();
+        String inputLine = sc.nextLine();
+        StringTokenizer tokenizer = new StringTokenizer(inputLine);
+        while (tokenizer.hasMoreTokens()) {
+        	keywordsList.add(tokenizer.nextToken());
+        }
+        String[] keywords = new String[keywordsList.size()];
+        keywordsList.toArray(keywords);
+        
+        System.out.println("Searching keywords:");
+        for (String k:keywords) {
+        	System.out.println(k);
+        }
+        
 		
 		final String INPUT_DIR = "resources\\W3C Web Pages\\Text\\";
 		final boolean REVERSE_COMPARE = true;
+		final int LIST_TOP_N_SCORE = 10;
 
 		BinaryHeap<FileScore> heap = new BinaryHeap<FileScore>();
 		
@@ -42,15 +63,17 @@ public class WebSearchEngine {
 		File[] directoryListing = dir.listFiles();
 		if (directoryListing != null) {
 		  for (File file : directoryListing) {
-		  	int score = GetFileMatchScore(file.getAbsolutePath(), args);
+		  	int score = GetFileMatchScore(file.getAbsolutePath(), keywords);
 		  	heap.insert(new FileScore(file.getName(), score, REVERSE_COMPARE));
 		  }
 		}
 		
+		System.out.println(String.format("-----------Top %d returned pages-----------:", LIST_TOP_N_SCORE));
+		
 		FileScore fs = null;
-		while ((fs = heap.deleteMin()) != null) {
+		int i = 0;
+		while ((fs = heap.deleteMin()) != null && i++ < LIST_TOP_N_SCORE) {
 			System.out.println(String.format("%d\t%s", fs.getScore(), fs.getFile()));
-			
 		}
 	}
 }
