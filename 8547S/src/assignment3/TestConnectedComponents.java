3663fad65b7a7a2e309f157970ce2d1104fedd25
Wen Dong <wen.dong@microfocus.com>
2020-10-31 15:00:18 +0200

    task 3

--- 8547S/src/assignment3/TestConnectedComponents.java
+++ 8547S/src/assignment3/TestConnectedComponents.java
@@ -1,29 +1,30 @@
 package assignment3;
 
-import java.io.File;
-
 import graphs.CC;
 import graphs.Graph;
-import graphs.In;
 import graphs.Queue;
-import graphs.StdOut;
+import graphs.SymbolGraph;
 
 public class TestConnectedComponents {
 
 	public static void main(String[] args) {
 		String dgFileName = "movies.txt";
-		In in = new In(new File(TestDfS.INPUT_DATA_DIR + dgFileName));
-		
-        Graph G = new Graph(in);
+		SymbolGraph sg = new SymbolGraph(TestDfS.INPUT_DATA_DIR + dgFileName, "/");
+        Graph G = sg.G();
+        
+        
+        long start = System.currentTimeMillis();
         CC cc = new CC(G);
 
         // number of connected components
-        int M = cc.count();
-        StdOut.println(M + " components");
+        int componentsNumber = cc.count();
+        long end = System.currentTimeMillis();
+
+        System.out.println(componentsNumber + " components");
 
         // compute list of vertices in each connected component
-        Queue<Integer>[] components = (Queue<Integer>[]) new Queue[M];
-        for (int i = 0; i < M; i++) {
+        Queue<Integer>[] components = (Queue<Integer>[]) new Queue[componentsNumber];
+        for (int i = 0; i < componentsNumber; i++) {
             components[i] = new Queue<Integer>();
         }
         for (int v = 0; v < G.V(); v++) {
@@ -31,13 +32,15 @@ public class TestConnectedComponents {
         }
 
         // print results
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < componentsNumber; i++) {
             for (int v : components[i]) {
-                StdOut.print(v + " ");
+            	System.out.print(v + " ");
             }
-            StdOut.println();
+            System.out.println();
         }
-
+        
+        System.out.println(componentsNumber + " components in all.");
+        System.out.println(String.format("It took %d ms to use DFS identifying the components.", end-start));
 	}
 
 }
