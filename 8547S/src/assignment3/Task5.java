758c46bce8f4d2fb2849eab16d5026e2034d81bc
Wen Dong <dong23@uwindsor.ca>
2020-11-07 11:49:02 +0800

    task 5

--- 8547S/src/assignment3/Task5.java
+++ 8547S/src/assignment3/Task5.java
@@ -6,38 +6,100 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import heaps.BinaryHeap;
 import memoryManagement.*;
 
 public class Task5 {
+	
+	public static void sortFile(String inputFile, String outputFile) throws IOException {
+		System.out.println("Sorting " + inputFile + " as " + outputFile + ".");
+		BinaryHeap<String> h = new BinaryHeap<String>();
+		try {
+			In in = new In(inputFile);
+            while (!in.isEmpty()) {
+                String s = in.readLine();
+                h.insert(s);
+            }
+        }
+        catch (Exception e) { System.out.println(e); }
+		
+		FileWriter fw = new FileWriter(outputFile);
+		while ( !h.isEmpty() ) {
+			String min = h.deleteMin();
+			fw.write(min + "\n");
+		}
+		fw.close();
+	}
+	
+	public static void multiWay(String[] files, String outputFile) throws IOException {
+		System.out.println("Merge sorting following files:");
+		for (String f:files) {
+			System.out.println(f);
+		}
+		int N = files.length; 
+        In[] streams = new In[N]; 
+        for (int i = 0; i < N; i++) 
+            streams[i] = new In(files[i]);
+        
+        IndexMinPQ<String> pq = new IndexMinPQ<String>(N); 
+        for (int i = 0; i < N; i++) 
+            if (!streams[i].isEmpty()) 
+                pq.insert(i, streams[i].readString()); 
+        
+        FileWriter fw = new FileWriter(outputFile);
+        while (!pq.isEmpty()) {
+        	fw.write(pq.minKey() + "\n");
+            int i = pq.delMin(); 
+            if (!streams[i].isEmpty()) 
+                pq.insert(i, streams[i].readString()); 
+        }
+        fw.close();
+        System.out.println("Merge Sorting done into '" + outputFile + "'");
+	}
 
 	public static void main(String[] args) throws IOException {
 		final String sourceDateFile = "resources/ChIP-seq reads/ChIP-seq-reads-1M.dat";
-		String[] sublists = { "A.dat", "B.dat", "C.dat", "D.dat" };
+		final String OutputDir = "output/";
+		final String sortedFile = OutputDir + "Chip-seq-reads-1M-sorted.dat";
 		
+		String[] sublists = { "A.dat", "B.dat", "C.dat", "D.dat" };
+		String[] sortedFiles = { "AS.dat", "BS.dat", "CS.dat", "DS.dat" };
+		
+		for (int i=0; i<sublists.length; i++) {
+			sublists[i] = OutputDir + sublists[i];
+			sortedFiles[i] = OutputDir + sortedFiles[i];
+		}
+		
+		System.out.println("Partitioning '" + sourceDateFile + "' into following files:");
+		for (String f:sublists) {
+			System.out.println(f);
+		}
 		HashMap<String, FileWriter> outputs = new HashMap<String, FileWriter>();
 		for (String f:sublists) {
 			outputs.put(f, new FileWriter(f));
 		}
 
-		int mod = sublists.length;
-		int c = 0;
-		In in;
 		try {
-            in = new In(sourceDateFile);
-            while (!in.isEmpty() && c < 100) {
-                String s = in.readLine();
-                outputs.get(sublists[c % mod]).write(s);
-                c++;
+			int mod = sublists.length;
+			int c = 0;
+			In in = new In(sourceDateFile);
+			FileWriter fw;
+            while (!in.isEmpty()) {
+                String s = in.readLine().trim();
+                fw = outputs.get(sublists[c++ % mod]);
+                fw.write(s + "\n");
             }
         }
         catch (Exception e) { System.out.println(e); }
-        System.out.println("Done");
-        
-        Iterator it = outputs.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            pair.getValue()
-        }
+		
+		for (String f:sublists) {
+			outputs.get(f).close();
+		}
+		
+		for (int i=0; i<sublists.length; i++) {
+			sortFile(sublists[i], sortedFiles[i]);
+		}
+		
+		multiWay(sortedFiles, sortedFile);
 	}
-
 }
