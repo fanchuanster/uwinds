a3cd0cfdbc30ba387539578e78a1c244307456c5
Wen Dong <wen.dong@microfocus.com>
2020-11-16 16:03:17 +0800

    task4 done

--- 8547S/src/assignment4/Task4.java
+++ 8547S/src/assignment4/Task4.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 import common.util;
 
 public class Task4 {
@@ -27,17 +29,17 @@ public class Task4 {
 
 	public static void main(String[] args) {
 		final Pattern EMAIL_ADDRESS_PATTERN = 
-			    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-		final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$" 
-				+ "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$" 
-				+ "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$");
+			    Pattern.compile("[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}", Pattern.CASE_INSENSITIVE);
+		final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}" 
+				+ "|(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}" 
+				+ "|(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}");
 		
 		final String inputTextDir = "output/";
 		File directory = new File(inputTextDir);
 		File[] files = directory.listFiles();
 		
-		Set<String> emailsSet = new HashSet<String>();
-		Set<String> phoneNumberSet = new HashSet<String>();
+		ArrayList<String> emailsList = new ArrayList<String>();
+		ArrayList<String> phoneNumberList = new ArrayList<String>();
 		for ( File f : files) {
 			if (!f.isFile()) {
 				continue;
@@ -45,15 +47,18 @@ public class Task4 {
 			
 			String[] emails = findPattern(f, EMAIL_ADDRESS_PATTERN);
 			for (String email:emails) {
-				emailsSet.add(email);
+				emailsList.add(email);
 			}
 			String[] phoneNumbers = findPattern(f, PHONE_NUMBER_PATTERN);
 			for (String phoneNumber:phoneNumbers) {
-				phoneNumberSet.add(phoneNumber);
+				phoneNumberList.add(phoneNumber);
 			}
 		}
 		
-		System.out.println("Found unique emails " + emailsSet.size());
-		System.out.println("Found unique phone numbers " + phoneNumberSet.size());
+		ArrayList<String> uniqueEmails = (ArrayList) emailsList.stream().distinct().collect(Collectors.toList());
+		ArrayList<String> uniquePhoneNumbers = (ArrayList) phoneNumberList.stream().distinct().collect(Collectors.toList());
+		System.out.println(String.format("Found %d emails, %d unique emails:\n%s", emailsList.size(), uniqueEmails.size(), uniqueEmails.toString()));
+		System.out.println();
+		System.out.println(String.format("Found %d phone numbers, %d unique phone numbers:\n%s", phoneNumberList.size(),uniquePhoneNumbers.size(), uniquePhoneNumbers.toString()));
 	}
 }
