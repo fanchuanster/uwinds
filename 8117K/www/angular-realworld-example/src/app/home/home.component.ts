a63fd3e355e9c5f18d0f4544046eec880c2eedb3
Wen Dong <wen.dong@microfocus.com>
2020-11-25 17:36:19 +0800

    test

--- 8117K/www/angular-realworld-example/src/app/home/home.component.ts
+++ 8117K/www/angular-realworld-example/src/app/home/home.component.ts
@@ -19,23 +19,46 @@ export class HomeComponent {
     private apiService: ApiService,
   ) {}
 
-  res; 
+  key = "AIzaSyARJnJeSRuwWaW9Y4LMBnV8MqwaybrRKq0"
+  sessiontoken = '1234567890'
+  timezone; 
 
-  auto_list: City[];
-  get(input) {
-    console.log('get ' + input);
-    this.apiService.get('/place/autocomplete/json?input='+ encodeURI(input) + '&key=AIzaSyARJnJeSRuwWaW9Y4LMBnV8MqwaybrRKq0&sessiontoken=1234567890')
+  predictions;
+
+  getTimeZoneInfo(loc) {
+    // https://maps.googleapis.com/maps/api/timezone/json?location=31.230416,121.473701&timestamp=1331161200&key=AIzaSyARJnJeSRuwWaW9Y4LMBnV8MqwaybrRKq0
+    this.apiService.get('/timezone/json?location='+ encodeURI(loc.lat+","+loc.lng) + '&timestamp=1331161200&key='+this.key+'&sessiontoken='+this.sessiontoken)
+      .subscribe(
+        data => {
+          this.timezone = data.timeZoneName + " @" + data.rawOffset/3600;
+          return data;
+        });
+  }
+
+  getPlaceInfo(predictions) {
+    this.timezone = "";
+    if (predictions.length > 0) {
+      var placeId = predictions[0].place_id;
+      // https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJMzz1sUBwsjURoWTDI5QSlQI&fields=geometry&key=AIzaSyARJnJeSRuwWaW9Y4LMBnV8MqwaybrRKq0
+      this.apiService.get('/place/details/json?place_id='+ encodeURI(placeId) + '&fields=geometry&key='+this.key+'&sessiontoken='+this.sessiontoken)
+      .subscribe(
+        data => {
+          var loc = data.geometry.location;
+          this.getTimeZoneInfo(loc);
+          return data;
+        });
+    }
+  }
+
+  onSearchChange(searchValue: string): void {  
+    console.log('get ' + searchValue);
+    this.apiService.get('/place/autocomplete/json?input='+ encodeURI(searchValue) + '&key='+this.key+'&sessiontoken='+this.sessiontoken)
     .subscribe(
       data => {
-        this.res = data.predictions;
-        console.log(data);
+        this.predictions = data.predictions;
+        this.getPlaceInfo(this.predictions);
         return data;
       });
-  }
-  onSearchChange(searchValue: string): void {  
-    console.log(searchValue);
-    // console.log();
-    this.get(searchValue);
   }
   time = new Observable<string>(observer => {
     setInterval(() => observer.next(new Date().toString()), 1000);
--- 8117K/www/python/flask_s.py
+++ 8117K/www/python/flask_s.py
@@ -9,7 +9,36 @@ app.logger.setLevel(logging.INFO)
 app.logger.info("some text")
 
 google_api = 'https://maps.googleapis.com'
+
 autocomplete_path = '/maps/api/place/autocomplete/json'
+placedetails_path = '/maps/api/place/details/json'
+timezone_path = '/maps/api/timezone/json'
+
+@app.route(timezone_path)
+def timezone():
+	app.logger.info('timezone')
+	location = request.args.get('location')
+	timestamp = request.args.get('timestamp')
+	key = request.args.get('key')
+	sessiontoken = request.args.get('sessiontoken')
+	redirect_url = google_api + timezone_path + "?location=%s&timestamp=%s&key=%s&sessiontoken=%s" % (location, timestamp, key, sessiontoken)
+	x = requests.get(redirect_url)
+
+	app.logger.info(x.json())
+	return x.json()
+
+@app.route(placedetails_path)
+def placedetails():
+	app.logger.info('placedetails')
+	place_id = request.args.get('place_id')
+	fields = request.args.get('fields')
+	key = request.args.get('key')
+	sessiontoken = request.args.get('sessiontoken')
+	redirect_url = google_api + placedetails_path + "?place_id=%s&fields=%s&key=%s&sessiontoken=%s" % (place_id, fields, key, sessiontoken)
+	x = requests.get(redirect_url)
+
+	# app.logger.info(x.json())
+	return x.json()['result']
 
 @app.route(autocomplete_path)
 def autocomplete():
@@ -20,7 +49,7 @@ def autocomplete():
 	redirect_url = google_api + autocomplete_path + "?input=%s&key=%s&sessiontoken=%s" % (input, key, sessiontoken)
 	x = requests.get(redirect_url)
 
-	app.logger.info(x.json())
+	# app.logger.info(x.json())
 	return x.json()
    
 # @app.route('/', defaults={'path': ''})
