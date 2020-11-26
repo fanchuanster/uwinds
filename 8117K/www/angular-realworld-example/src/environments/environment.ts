292103aef74e2aa05fcdf3b76938a289dd16fba9
Wen Dong <wen.dong@microfocus.com>
2020-11-26 14:33:18 +0800

    test

--- 8117K/www/angular-realworld-example/src/environments/environment.ts
+++ 8117K/www/angular-realworld-example/src/environments/environment.ts
@@ -7,6 +7,5 @@ export const environment = {
   production: false,
   // api_url: 'https://conduit.productionready.io/api'
   // api_url: 'https://maps.googleapis.com/maps/api'
-  // api_url: 'http://localhost:4200/maps/api'
   api_url: 'http://169.48.25.194:5000/maps/api'
 };
